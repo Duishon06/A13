2481b49ba730533a28bf258f9aef2c1634e53f0c
Dushobai <asyrbekovdusobaj@gmail.com>
2025-01-28 10:53:16 +0600

    add git command

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
-let box = document.querySelector (".box")
+// let box = document.querySelector (".box")
 
-box.innerHTML = "Hello Aidai"
+// box.innerHTML = "Hello Aidai"
 
-let content = document.querySelector(".contetnt")
+// let content = document.querySelector(".contetnt")
 
-content.innerHTML = "Hello Beknur"
+// content.innerHTML = "Hello Beknur"
+
+
+
+function checkName() {
+    let age = (prompt("Жашыңыз канчада?")); 
+
+    if (age >= 18) {
+        alert("Сизге кирүүгө болот.");
+    } else if (age < 18) {
+        let setName = confirm("Ата-энеңиз уруксат бердиби?"); 
+        if (setName) {
+            alert("Сизге кирүүгө болот."); 
+        } else {
+            alert("Сизге кирүүгө болбойт.");
+        }
+    }
+}
+checkName();
